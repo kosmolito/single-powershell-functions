26578a43ba8738799397acb87dba2caadd76de88
kosmolito <said.karker@gmail.com>
2022-12-28 09:37:12 +0100

    Bugfixes and more dynamic function

--- creating-ad-users.ps1
+++ creating-ad-users.ps1
@@ -1,14 +1,14 @@
 
-function Add-KSADUser {
+function New-KSADUser {
     [CmdletBinding()]
     param(
-        [Parameter][string]$FirstName,
-        [Parameter][String]$LastName,
-        [Parameter][String]$UserPassword,
-        [Parameter][String]$OU,
-        [Parameter][String]$DomainName,
-        [Parameter][string]$SecurityGroups,
-        [Parameter][String]$UserName
+        [Parameter()][string]$FirstName,
+        [Parameter()][String]$LastName,
+        [Parameter()][String]$UserPassword,
+        [Parameter()][String]$OU,
+        [Parameter()][String]$DomainName,
+        [Parameter()][string]$SecurityGroups,
+        [Parameter()][String]$UserName
         )
 
     $FullName = $FirstName + " " + $LastName
@@ -37,16 +37,10 @@ function Add-KSADUser {
     $UserName = $TempUserName
 
 
-    # If is not porvided, the password will be set as Mov2022
-    if ($UserPassword) {
-        $UserPassword = ConvertTo-SecureString $UserPassword -AsPlainText -Force
-    } else {
-        $UserPassword = ConvertTo-SecureString "Move2022" -AsPlainText -Force
-    }
-
     $SecurityGroups = $SecurityGroups.split(",")
     $DomainDistinguishedName = (get-addomain).distinguishedname
-    
+
+    if ($PSBoundParameters.ContainsKey("UserOU")) {
     $UserOU = "Users"    
     if (-not (Get-ADOrganizationalUnit -Filter 'name -like $OU'))
         { New-ADOrganizationalUnit -Name $OU -Path "$DomainDistinguishedName" -ProtectedFromAccidentalDeletion $false }
@@ -59,26 +53,40 @@ function Add-KSADUser {
     if (-not (Get-ADOrganizationalUnit -filter 'name -like $UserOU' | Where-Object {$_.DistinguishedName -match "OU=$OU,$DomainDistinguishedName"}) ) 
                 { New-ADOrganizationalUnit -Name $UserOU -Path "OU=$OU,$DomainDistinguishedName" -ProtectedFromAccidentalDeletion $false }
     $UserOUPath = "OU=$UserOU,OU=$OU,$DomainDistinguishedName"
+    } else {
+        $UserOUPath = "CN=Users,$DomainDistinguishedName"
+    }
 
-    # Adding One extra SEC Group for each OU to easier NTFS target
-    $SecurityGroups = $SecurityGroups + "SEC_$OU"
 
-    $TempSecGroups = $null
-    foreach ($Group in $SecurityGroups) {
-        if (!($Group -match "^SEC_")) {
-            $Group = "SEC_" + $Group
+    if($PSBoundParameters.ContainsKey("SecurityGroups")) {
+
+        # Adding One extra SEC Group for each OU to easier NTFS target
+        $SecurityGroups = $SecurityGroups + "SEC_$OU"
+
+        $TempSecGroups = $null
+        foreach ($Group in $SecurityGroups) {
+            if (!($Group -match "^SEC_")) {
+                $Group = "SEC_" + $Group
+            }
+            [array]$TempSecGroups = $TempSecGroups + $Group
         }
-        [array]$TempSecGroups = $TempSecGroups + $Group
-    }
-    
-    $SecurityGroups = $TempSecGroups
+        
+        $SecurityGroups = $TempSecGroups
 
-    foreach ($SecurityGroup in $SecurityGroups) {
-        if (-not (Get-ADGroup -Filter 'Name -like $SecurityGroup')) 
-        { New-ADGroup -Name $SecurityGroup -GroupCategory Security -GroupScope Global -Path "OU=$SecurityGroupOU,$DomainDistinguishedName" }    
+        foreach ($SecurityGroup in $SecurityGroups) {
+            if (-not (Get-ADGroup -Filter 'Name -like $SecurityGroup')) 
+            { New-ADGroup -Name $SecurityGroup -GroupCategory Security -GroupScope Global -Path "OU=$SecurityGroupOU,$DomainDistinguishedName" }    
+        }
     }
 
-    New-AdUser -AccountPassword $UserPassword `
+    # If Password not provided, the password will be set as Mov2022
+    if ($PSBoundParameters.ContainsKey("UserPassword")) {
+        $Password = ConvertTo-SecureString $UserPassword -AsPlainText -Force
+    } else {
+        $Password = ConvertTo-SecureString "Move2022" -AsPlainText -Force
+    }
+
+    New-AdUser -AccountPassword $Password `
     -GivenName $FirstName `
     -Surname $LastName `
     -DisplayName $FullName `
@@ -89,8 +97,11 @@ function Add-KSADUser {
     -Path $UserOUPath `
     -Enabled $true
 
-    # -Path "ou=$OU,$(([ADSI]`"").distinguishedName)" `
-    foreach ($SecurityGroup in $SecurityGroups) {
-        Add-ADGroupMember -Identity $SecurityGroup -Members $username
+    # Add User to Security Group if Security Grup information is provided
+    if ($PSBoundParameters.ContainsKey("SecurityGroups")) {
+        # -Path "ou=$OU,$(([ADSI]`"").distinguishedName)" `
+        foreach ($SecurityGroup in $SecurityGroups) {
+            Add-ADGroupMember -Identity $SecurityGroup -Members $username
+        }
     }
 }
